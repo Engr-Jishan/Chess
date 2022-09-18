6a95a432ceba7f3f63d9e7d7389bd3fd4b6124b1
jisha66 <mydreamplace2020@gmail.com>
2022-09-18 22:44:09 +0600

    third page

--- chess.js
+++ chess.js
@@ -16,3 +16,28 @@ function insertImage() {
         }
     })
 }
+insertImage()
+
+// Coloring
+function Coloring() {
+    const color = document.querySelectorAll('.box')
+
+    color.forEach(color => {
+
+        getId = color.id
+        arr = Array.from(getId)
+        arr.shift()
+        aside = eval(arr.pop())
+        aup = eval(arr.shift())
+        a = aside + aup
+
+        if (a % 2 == 0) {
+            color.style.backgroundColor = 'rgb(240, 201, 150)'
+        }
+        if (a % 2 !== 0) {
+            color.style.backgroundColor = 'rgb(100, 75, 45)'
+        }
+    })
+}
+Coloring()
+
